3ed0a57229316011d2280a15b38fb6a2f4634b77
Eunjin Woo <esther0904@naver.com>
2023-08-30 16:53:13 +0900

    lv1 12

---
+++ Programmers/Lv.1/12.py
@@ -0,0 +1,10 @@
+# 두 정수 사이의 합
+
+def solution(a, b):
+    answer = 0
+    a, b = min([a,b]), max([a,b])
+    for i in range(a, b+1):
+        answer += i
+    return answer
+
+# return sum(range(a, b + 1)) 이렇게도 가능.
